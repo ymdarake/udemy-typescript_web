32d39f5dc993e31edb8ca1990179f86ef1ed8fb0
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-12 17:50:31 +1300

    [185-188]View: Rendering UserForm

---
+++ src/views/UserForm.ts
@@ -0,0 +1,20 @@
+export class UserForm {
+  constructor(public parent: Element) {}
+
+  template(): string {
+    return `
+			<div>
+				<h1>User Form</h1>
+				<input />
+			</div>
+		`;
+  }
+
+  // appending to the parent
+  render(): void {
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = this.template();
+
+    this.parent.append(templateElement.content);
+  }
+}
