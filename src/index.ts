ef3a6a046c3eea806e63d4bfbf5bc52d35db5ad0
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-12 16:46:09 +1300

    [171]Setting Date while Triggering Event

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ name: 'new record', age: 0 });
+
+user.on('change', () => {
+  console.log('user was changed, we probaly need to update some HTML');
+});
+
+user.set({ name: 'FOoo' });
