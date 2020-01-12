d68a27a2c5e8f8df495fa084a350f1184f76f7b9
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-12 15:20:13 +1300

    [143-147]Eventing System

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,13 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 20 });
 
-user.set({ name: 'newname' });
-
 console.log(user.get('name'));
 console.log(user.get('age'));
+
+user.on('change', () => {
+  console.log(111);
+});
+
+user.set({ name: 'newname' });
+
+user.trigger('change');
