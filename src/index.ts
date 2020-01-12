45d9132b24a963604ca46bbc1ba8e6a00e178ea9
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-12 15:32:17 +1300

    [150]Adding Fetching Functionality

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
-import { User } from './models/User';
+import axios from 'axios';
 
-const user = new User({ name: 'myname', age: 20 });
-
-console.log(user.get('name'));
-console.log(user.get('age'));
-
-user.on('change', () => {
-  console.log(111);
-});
-
-user.set({ name: 'newname' });
-
-user.trigger('change');
+axios.get('http://localhost:3000/users/1');
