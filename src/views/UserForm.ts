b59c866672894d8f7a8d8434eb94c2eb0e2aaa78
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-12 18:02:47 +1300

    [190]Binding Events

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,13 +1,40 @@
 export class UserForm {
   constructor(public parent: Element) {}
 
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick,
+      'mouseenter:h1': this.onHeaderHover
+      // 'drag:div': this.onDragDiv
+    };
+  }
+
+  onButtonClick(): void {
+    console.log('Hi there');
+  }
+
+  onHeaderHover(): void {
+    console.log('H1 was hovered over');
+  }
+
   template(): string {
     return `
 			<div>
 				<h1>User Form</h1>
 				<input />
+				<button>click me</button>
 			</div>
 		`;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
   }
 
   // appending to the parent
@@ -15,6 +42,7 @@ export class UserForm {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
 
+    this.bindEvents(templateElement.content);
     this.parent.append(templateElement.content);
   }
 }
